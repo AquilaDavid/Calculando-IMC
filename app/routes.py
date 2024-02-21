c0f8bfa70eb17172cbee0af444bb6257d0276cf3
AquilaDavid <aquiladavid07@gmail.com>
2024-02-21 03:37:04 -0300

    Finalmente conseguir ativar o debug no meu codigo.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from doctest import debug
+from os import name
 from app import app
 from flask import render_template
 
@@ -8,8 +8,6 @@ from flask import render_template
 def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug = True)
 
 @app.route("/pagina")
 def pagina2 ():
