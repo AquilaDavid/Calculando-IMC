ce4d498aaddf5a3fa417a53c5b616d720b32b7c7
AquilaDavid <aquiladavid07@gmail.com>
2024-02-27 15:57:40 -0300

    preparando para o calculo do imc

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from os import name
+from urllib import request
+from xml.sax.handler import all_features
 from app import app
 from flask import render_template
+from flask import Flask, render_template,request,flash,redirect
 
 
 @app.route("/")
@@ -11,4 +14,11 @@ def index():
 
 @app.route("/pagina")
 def pagina2 ():
+    return render_template("pagina.html")
+
+def pagina ():
+    altura = request.form['altura']
+    peso = request.form['peso']
+    print(altura)
+    print(peso)
     return render_template("pagina.html")
